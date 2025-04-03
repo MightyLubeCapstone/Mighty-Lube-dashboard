71a96a3b7f7e2ead8193fb0b07d3bc1613ecc2dc
LucasDowlen <LucasColbyDowlen@gmail.com>
2025-04-03 10:05:30 -0500

    Initial commit: Added dashboard project files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Dashboard from './Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <Dashboard/>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function Dashboard() {
+  return (
+    <div className="Dashboard">
+      <header>
+        <p>Mighty Lube</p>
+      </header>
+    </div>
+  );
+}
+
+export default Dashboard;
